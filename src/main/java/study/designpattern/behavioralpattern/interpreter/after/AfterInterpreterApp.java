2fd1ee540ed4ceaa64052f72b1cd25a6e09576a2
javython999 <infinity2544@gmail.com>
2023-10-07 00:09:39 +0900

    Interpreter

---
+++ src/main/java/study/designpattern/behavioralpattern/interpreter/after/AfterInterpreterApp.java
@@ -0,0 +1,12 @@
+package study.designpattern.behavioralpattern.interpreter.after;
+
+import java.util.Map;
+
+public class AfterInterpreterApp {
+
+    public static void main(String[] args) {
+        PostfixExpression expression = PostfixParser.parse("xyz+-");
+        int result = expression.interpret(Map.of('x', 1, 'y', 2, 'z', 3));
+        System.out.println(result);
+    }
+}
---
+++ src/main/java/study/designpattern/behavioralpattern/interpreter/after/PostfixParser.java
@@ -0,0 +1,32 @@
+package study.designpattern.behavioralpattern.interpreter.after;
+
+import java.util.Stack;
+
+import static study.designpattern.behavioralpattern.interpreter.after.PostfixExpression.*;
+
+
+public class PostfixParser {
+
+    public static PostfixExpression parse(String expression) {
+        Stack<PostfixExpression> stack = new Stack<>();
+        for (char c : expression.toCharArray()) {
+            stack.push(getExpression(c, stack));
+        }
+        return stack.pop();
+    }
+
+    private static PostfixExpression getExpression(char c, Stack<PostfixExpression> stack) {
+        switch (c) {
+            case '+':
+                return plus(stack.pop(), stack.pop());
+            case '-':
+                PostfixExpression right = stack.pop();
+                PostfixExpression left = stack.pop();
+                return minus(left, right);
+            case '*':
+                return multiply(stack.pop(), stack.pop());
+            default:
+                return variable(c);
+        }
+    }
+}
