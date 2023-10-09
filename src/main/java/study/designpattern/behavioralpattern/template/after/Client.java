c2a85e51e8133cde5ae32cdcbfecd75cd0e39d96
javython999 <infinity2544@gmail.com>
2023-10-09 19:23:42 +0900

    template

---
+++ src/main/java/study/designpattern/behavioralpattern/template/after/Client.java
@@ -0,0 +1,10 @@
+package study.designpattern.behavioralpattern.template.after;
+
+public class Client {
+
+    public static void main(String[] args) {
+        FileProcessor fileProcessor = new FileProcessor("number.txt");
+        int result = fileProcessor.process((result1, number) -> result1 += number);
+        System.out.println(result);
+    }
+}
